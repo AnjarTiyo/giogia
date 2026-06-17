665e24fa61dfdf561aa1fb3b6946c8bfdc192ec6
AnjarTiyo <anjar.jog@gmail.com>
2026-06-17 18:17:08 +0700

    fix: make cli dist executable, add tsup banner+chmod hook

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -6,4 +6,9 @@ export default defineConfig({
   dts: true,
   clean: true,
   sourcemap: true,
+  banner: { js: '#!/usr/bin/env node' },
+  async onSuccess() {
+    const { execSync } = await import('node:child_process')
+    execSync('chmod +x dist/index.js')
+  },
 })
